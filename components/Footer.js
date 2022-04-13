c8e29837f97d06d3b808afa0d923cc6302910e7c
Rui Guilherme <guilhermecunha.rui@gmail.com>
2022-04-13 14:22:14 -0300

    update locale on the same page
    changeLanguage simply don't work here.

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Grid } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import Link from 'next/link'
@@ -7,19 +7,26 @@ import Link from 'next/link'
 export default function Footer() {
 	const { t } = useTranslation('common')
 
+	const [actualPath, setActualPath] = useState('/')
+
+	useEffect(() => {
+		// remove pt, en and es from the path
+		setActualPath(window.location.pathname.replace(/\/pt|\/en|\/es/g, ''))
+	}, [])
+
 	return (
 		<Grid container item direction="row" justifyContent="center" alignItems="center" lg={12} md={12} sm={12} xl={12} xs={12}>
-			<Link href="/" locale="en" passHref>
+			<Link href={actualPath} locale="en" passHref>
 				<Button variant="outlined" aria-label={t('change-language-en')}>
 					{t('change-language-en')}
 				</Button>
 			</Link>
-			<Link href="/" locale="es" passHref>
+			<Link href={actualPath} locale="es" passHref>
 				<Button variant="outlined" aria-label={t('change-language-es')}>
 					{t('change-language-es')}
 				</Button>
 			</Link>
-			<Link href="/" locale="pt" passHref>
+			<Link href={actualPath} locale="pt" passHref>
 				<Button variant="outlined" aria-label={t('change-language-pt')}>
 					{t('change-language-pt')}
 				</Button>
